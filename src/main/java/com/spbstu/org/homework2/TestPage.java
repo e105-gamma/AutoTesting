2cc8d4a51ff55155f58276025832c1977b834b6a
Unknown <ksenobite95@gmail.com>
2018-06-04 14:50:50 +0300

    поставил везде 4 пробела

--- src/main/java/com/spbstu/org/homework2/TestPage.java
+++ src/main/java/com/spbstu/org/homework2/TestPage.java
@@ -9,82 +9,84 @@ import java.util.List;
 
 public class TestPage {
 
-  public WebDriver driver;
+    public WebDriver driver;
 
-  @FindBy(css = "form .btn-login")
-  WebElement loginButton;
+    @FindBy(css = "form .btn-login")
+    WebElement loginButton;
 
-  @FindBy(css = "#Login")
-  WebElement loginTextBox;
+    @FindBy(css = "#Login")
+    WebElement loginTextBox;
 
-  @FindBy(css = "#Password")
-  WebElement passwordTextBox;
+    @FindBy(css = "#Password")
+    WebElement passwordTextBox;
 
-  @FindBy(css = ".logout")
-  WebElement logoutButton;
+    @FindBy(css = ".logout")
+    WebElement logoutButton;
 
-  @FindBy(css = ".main-txt")
-  WebElement mainText;
+    @FindBy(css = ".main-txt")
+    WebElement mainText;
 
-  @FindBy(css = ".main-title")
-  WebElement mainTitle;
+    @FindBy(css = ".main-title")
+    WebElement mainTitle;
 
-  @FindBy(css = ".icons-benefit")
-  List<WebElement> icons;
+    @FindBy(css = ".icons-benefit")
+    List<WebElement> icons;
 
-  @FindBy(css = ".benefit-txt")
-  List<WebElement> benefitTexts;
+    @FindBy(css = ".benefit-txt")
+    List<WebElement> benefitTexts;
 
-  @FindBy(css = ".profile-photo")
-  WebElement profileIcon;
+    @FindBy(css = ".profile-photo")
+    WebElement profileIcon;
 
-  public TestPage(WebDriver driver) {
-    this.driver = driver;
-  }
+    public TestPage(WebDriver driver) {
+        this.driver = driver;
+    }
 
-  public String getUsername() {
-    return profileIcon.getText();
-  }
+    public String getUsername() {
+        return profileIcon.getText();
+    }
 
-  public int getIconsNumber() {
-    return icons.size();
-  }
+    public int getIconsNumber() {
+        return icons.size();
+    }
 
-  // not used
-  public String getBenefitText(int i) {
-    return benefitTexts.get(i).getText();
-  }
+    // not used
+    public String getBenefitText(int i) {
+        return benefitTexts.get(i).getText();
+    }
 
-  public List<String> getBenefitTexts() {
-    List<String> list = new ArrayList<>();
-    for (WebElement benefitText : benefitTexts) list.add(benefitText.getText());
-    return list;
-  }
+    public List<String> getBenefitTexts() {
+        List<String> list = new ArrayList<>();
+        for (WebElement benefitText : benefitTexts) list.add(benefitText.getText());
+        return list;
+    }
 
-  public String getMainTitle() {
-    return mainTitle.getText();
-  }
 
-  public String getMainText() {
-    return mainText.getText();
-  }
 
-  public String getUrl() {
-    return driver.getCurrentUrl();
-  }
+    public String getMainTitle() {
+        return mainTitle.getText();
+    }
 
-  public String getTitle() {
-    return driver.getTitle();
-  }
+    public String getMainText() {
+        return mainText.getText();
+    }
 
-  public void login(String login, String password) {
-    profileIcon.click();
-    loginTextBox.sendKeys(login);
-    passwordTextBox.sendKeys(password);
-    loginButton.click();
-  }
+    public String getUrl() {
+        return driver.getCurrentUrl();
+    }
 
-  public void openPage(String URL) {
-    driver.navigate().to(URL);
-  }
+    public String getTitle() {
+        return driver.getTitle();
+    }
+
+    public void login(String login, String password) {
+        profileIcon.click();
+        loginTextBox.sendKeys(login);
+        passwordTextBox.sendKeys(password);
+        loginButton.click();
+    }
+
+    public void openPage(String URL) {
+        driver.navigate().to(URL);
+    }
 }
--- src/test/java/com/spbstu/org/homework2/HomeWork2.java
+++ src/test/java/com/spbstu/org/homework2/HomeWork2.java
@@ -12,64 +12,62 @@ import java.util.List;
 
 public class HomeWork2 {
 
-  WebDriver driver;
-  public static TestPage testPage;
+    WebDriver driver;
+    public static TestPage testPage;
 
-  @BeforeMethod
-  public void init() {
+    @BeforeMethod
+    public void init() {
 
-    System.setProperty("webdriver.chrome.driver", "chromedriver");
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--start-maximized");
-    driver = new ChromeDriver(options);
-    testPage = PageFactory.initElements(driver, TestPage.class);
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        testPage = PageFactory.initElements(driver, TestPage.class);
   }
 
-  //    @AfterTest
-  //    public void after() {
-  //        TestWebsite.homePage.driver.close();
-  //    }
 
-  @Test
-  public void testHomePage() {
 
-    // go to URL
-    testPage.openPage(TestData.TARGET_URL);
+    @Test
+    public void testHomePage() {
 
-    // test title
-    Assert.assertEquals(TestData.WEBSITE_TITLE, testPage.getTitle(), "Title doesn't match.");
+        // go to URL
+        testPage.openPage(TestData.TARGET_URL);
 
-    // login in
-    testPage.login(TestData.LOGIN, TestData.PASSWORD);
+        // test title
+        Assert.assertEquals(TestData.WEBSITE_TITLE, testPage.getTitle(), "Title doesn't match.");
 
-    // test user name
-    Assert.assertEquals(testPage.getUsername(), TestData.USERNAME);
+        // login in
+        testPage.login(TestData.LOGIN, TestData.PASSWORD);
 
-    // test title again
-    Assert.assertEquals(testPage.getTitle(), TestData.WEBSITE_TITLE);
+        // test user name
+        Assert.assertEquals(testPage.getUsername(), TestData.USERNAME);
 
-    // test 4 display images
-    Assert.assertEquals(
-        testPage.getIconsNumber(),
-        TestData.NUMBER_OF_IMAGES,
-        "The number of images does not match the target.");
+        // test title again
+        Assert.assertEquals(testPage.getTitle(), TestData.WEBSITE_TITLE);
 
-    // test 4 texts
-    List<String> benefitsList = testPage.getBenefitTexts();
 
-    Assert.assertEquals(
-        benefitsList.size(),
-        TestData.NUMBER_OF_TEXTS,
-        String.format(
-            "The number of texts does not match the target.",
+        // test 4 display images
+        Assert.assertEquals(
+            testPage.getIconsNumber(),
+            TestData.NUMBER_OF_IMAGES,
+            "The number of images does not match the target.");
+
+        // test 4 texts
+        List<String> benefitsList = testPage.getBenefitTexts();
+
+        Assert.assertEquals(
             benefitsList.size(),
-            TestData.NUMBER_OF_TEXTS));
-    benefitsList.forEach(
-        textIterator ->
-            Assert.assertTrue(TestData.BENEFITS_TEXTS.contains(textIterator.toString())));
+            TestData.NUMBER_OF_TEXTS,
+            String.format(
+                "The number of texts does not match the target.",
+                benefitsList.size(),
+                TestData.NUMBER_OF_TEXTS));
+        benefitsList.forEach(
+            textIterator ->
+                Assert.assertTrue(TestData.BENEFITS_TEXTS.contains(textIterator.toString())));
 
-    // test main header
-    Assert.assertEquals(testPage.getMainTitle(), TestData.MAIN_HEADER);
-    Assert.assertEquals(testPage.getMainText(), TestData.HEADERS);
-  }
+        // test main header
+        Assert.assertEquals(testPage.getMainTitle(), TestData.MAIN_HEADER);
+        Assert.assertEquals(testPage.getMainText(), TestData.HEADERS);
+    }
 }
